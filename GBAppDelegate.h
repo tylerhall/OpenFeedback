1e7ab35a10dec2292112cab51cb4bd1064abd10e
Tomaz Kragelj <tkragelj@gmail.com>
2010-07-21 19:23:57 +0800

    Added testing application for simpler testing and showcase.

---
+++ GBAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GBAppDelegate.h
+//  OpenFeedback
+//
+//  Created by Tomaz Kragelj on 21.7.10.
+//  Copyright (C) 2010, Tomaz Kragelj. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class OpenFeedback;
+
+@interface GBAppDelegate : NSObject {
+	IBOutlet OpenFeedback *openFeedback;
+}
+
+@end
---
+++ GBAppDelegate.m
@@ -0,0 +1,14 @@
+//
+//  GBAppDelegate.m
+//  OpenFeedback
+//
+//  Created by Tomaz Kragelj on 21.7.10.
+//  Copyright (C) 2010, Tomaz Kragelj. All rights reserved.
+//
+
+#import <OpenFeedback/OpenFeedback.h>
+#import "GBAppDelegate.h"
+
+@implementation GBAppDelegate
+
+@end
